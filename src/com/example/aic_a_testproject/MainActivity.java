5fbe7cfbbc78559f955037b6f3c9811137aeb9ab
Akira Morikawa <alice0398@gmail.com>
2013-06-25 13:45:01 +0900

    no message

--- src/com/example/aic_a_testproject/MainActivity.java
+++ src/com/example/aic_a_testproject/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.aic_a_testproject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+private static final String TAG = "TAG";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.d(TAG,"(゜∀。)ﾜﾋｬﾋｬﾋｬﾋｬﾋｬﾋｬ")
+		Log.d(TAG,"(゜∀。)ﾜﾋｬﾋｬﾋｬﾋｬﾋｬﾋｬ");
 	}
 
 	@Override
